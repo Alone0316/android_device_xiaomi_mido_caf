71971b1a352c4c0ebec5c53735b494b70e934648
Alone0316 <rutujamandlik06@gmail.com>
2022-03-28 04:55:07 +0000

    mido: Fixup USB

--- device.mk
+++ device.mk
@@ -347,7 +347,7 @@ PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
     init.qcom.rc \
     init.qcom.sh \
-    init.qcom.usb.rc \
+    init.mido.usb.rc \
     init.recovery.qcom.rc \
     init.recovery.qcom.usb.rc \
     init.target.rc \
